4fb80273f547fbdfb7c187747fc3fb77046f32ea
suuny0321 <joosun7647@naver.com>
2025-01-13 15:29:03 +0900

    init

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "dev"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "suuny0321"
+}
+//환경변수 데이터들이 들어가 있는곳
